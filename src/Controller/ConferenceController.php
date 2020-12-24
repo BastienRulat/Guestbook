00abd16c2c143168b7f2129ac2b29bd1b148784c
BastienRulat <bastienrulat@gmail.com>
2020-12-24 15:46:46 +0100

    Add & configure database

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        /* Initial maker default code 
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
+        */
+
+        $greet = null;
+        if ($name = $request->query->get("hello"))
+            $greet = "Hello " . htmlspecialchars($name);
+       
+        var_dump($name);
+    
+        return new Response ('
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Homepage</title>
+            </head>
+            <body>
+                ' . $greet . '
+            </body>
+            </html>
+        ');
     }
 }
